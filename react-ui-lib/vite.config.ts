016d4c4343bb2464033821e1ff414f11980015de
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-09-02 02:42:14 +0530

    adding chnages in file

--- react-ui-lib/vite.config.ts
+++ react-ui-lib/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['src/**/*.stories.{js,ts,jsx,tsx}']
+  }
+});
